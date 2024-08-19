bc4dc87a86ac4dd42b2dc7b1139b43c647e15475
KrishKoria <krishkoria2004@gmail.com>
2024-08-19 22:21:48 +0530

    added rudimentry validation using react hook form

--- components/AddTransactionForm.tsx
+++ components/AddTransactionForm.tsx
@@ -20,7 +20,9 @@ export default function AddTransactionForm() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    mode: "onTouched",
+  });
 
   const onSubmit = (data: any) => console.log(data);
   return (
@@ -30,43 +32,77 @@ export default function AddTransactionForm() {
           <Label className="mb-1 block text-gray-700 dark:text-gray-300">
             Type
           </Label>
-          <Select {...register("type")}>
+          <Select
+            {...register("type", {
+              required: "Please select the type of transaction",
+            })}
+          >
             {types.map((category) => (
               <option key={category}>{category}</option>
             ))}
           </Select>
+          {errors.type && (
+            <p className="mt-1 text-red-600">{errors.type.message}</p>
+          )}
         </div>
 
         <div>
           <Label className="mb-1 block text-gray-700 dark:text-gray-300">
             Category
           </Label>
-          <Select {...register("category")}>
+          <Select
+            {...register("category", { required: "Please choose a category" })}
+          >
             {categories.map((category) => (
               <option key={category}>{category}</option>
             ))}
           </Select>
+          {errors.category && (
+            <p className="mt-1 text-red-600">{errors.category.message}</p>
+          )}
         </div>
 
         <div>
           <Label className="mb-1 block text-gray-700 dark:text-gray-300">
             Date
           </Label>
-          <Input {...register("created_at")} />
+          <Input
+            {...register("created_at", { required: "Please Enter a Date" })}
+          />
+          {errors.created_at && (
+            <p className="mt-1 text-red-600">{errors.created_at.message}</p>
+          )}
         </div>
 
         <div>
           <Label className="mb-1 block text-gray-700 dark:text-gray-300">
             Amount
           </Label>
-          <Input type="number" {...register("amount")} />
+          <Input
+            type="number"
+            {...register("amount", {
+              required: "Please Enter an Amount",
+              min: { value: 1, message: "Amount must be greater than 0" },
+              valueAsNumber: true,
+            })}
+          />
+          {errors.amount && (
+            <p className="mt-1 text-red-600">{errors.amount.message}</p>
+          )}
         </div>
 
-        <div className="col-span-2">
+        <div className="col-span-1 md:col-span-2">
           <Label className="mb-1 block text-gray-700 dark:text-gray-300">
             Description
           </Label>
-          <Input {...register("description")} />
+          <Input
+            {...register("description", {
+              required: "Please enter a description",
+            })}
+          />
+          {errors.description && (
+            <p className="mt-1 text-red-600">{errors.description.message}</p>
+          )}
         </div>
       </div>
 
